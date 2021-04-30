91f4738585c549b6a3e280eaa616f5e0e7f21130
kaiete <kaiete@aragon-press.com>
2021-04-30 18:52:04 +0100

    make it a bit more instant

--- instaloadr.js
+++ instaloadr.js
@@ -1,6 +1,12 @@
 async function fetchcss() {
+    if (localStorage.getItem("css") == null) {
     let css = await fetch("style.css")
     document.getElementById("il").innerHTML = await css.text()
-    return null
+    localStorage.setItem("css",await css.text())
+    return false
+    } else {
+        document.getElementById("il").innerHTML = localStorage.getItem("css")
+        return true
+    }
 }
 fetchcss()
